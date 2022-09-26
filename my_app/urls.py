8acb924e5e62cd770e0ab5c96b6fa83af64b785b
mfarzamalam <mfarzamalam@gmail.com>
2022-09-27 01:29:03 +0500

    update with dynamic templating

--- my_app/urls.py
+++ my_app/urls.py
@@ -2,4 +2,9 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
+    path('', index_view, name='index_view'),
+    path('about/', about_view, name='about_view'),
+    path('services/', services_view, name='services_view'),
+    path('accounts/', accounts_view, name='accounts_view'),
+    path('contact/', contact_view, name='contact_view'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home():...
+def index_view(request):
+    context = {'active':'index'}
+    return render(request, 'index.html', context)
 
-def about():...
 
-def contact():...
+def about_view(request):
+    context = {'active':'about'}
+    return render(request, 'about.html', context)
+
+
+def services_view(request):
+    context = {'active':'services'}
+    return render(request, 'services.html', context)
+
+
+def accounts_view(request):
+    context = {'active':'accounts'}
+    return render(request, 'accounts.html', context)
+
+
+def contact_view(request):
+    context = {'active':'contact'}
+    return render(request, 'contact.html', context)
