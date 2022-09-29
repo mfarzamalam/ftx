6b230fca2c4dfa51c93ea4bb44ded3d440353cab
raeesiqbal <rayiszafar@gmail.com>
2022-09-29 16:03:04 +0500

    d

--- static/master.js
+++ static/master.js
@@ -1,27 +1,23 @@
-$('.how-we-get').owlCarousel({
-	loop:true,
-	margin:10,
-	nav:true,
-	autoplay:true,
-	autoplayTimeout:2000,
-	autoplayHoverPause:true,
-	navText: ['<i class="text-white fa-solid fa-circle-arrow-left pe-2"></i>', '<i class="text-white fa-solid fa-circle-arrow-right"></i>'],
-	responsive:{
-		0:{
-			items:1
-		},
-		600:{
-			items:2
-		},
-		1000:{
-			items:2
-		}
-	}
+$(".how-we-get").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  autoplay: true,
+  autoplayTimeout: 4000,
+  autoplayHoverPause: true,
+  navText: [
+    '<i class="fa-solid fa-circle-arrow-left pe-2"></i>',
+    '<i class="fa-solid fa-circle-arrow-right"></i>',
+  ],
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 2,
+    },
+  },
 });
-
-
-
-
-
-
-
