e28c37366c97719e9f279ca3dc282a169b0e6d87
raeesiqbal <rayiszafar@gmail.com>
2022-10-05 16:22:33 +0500

    F

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
 from .views import *
+from .forms import *
 
 urlpatterns = [
     path('', index_view, name='index'),
@@ -7,4 +9,12 @@ urlpatterns = [
     path('services/', services_view, name='services'),
     path('accounts/', accounts_view, name='accounts'),
     path('contact/', contact_view, name='contact'),
+    path(
+        "login/",
+        LoginView.as_view(
+            template_name="login.html", authentication_form=UserLoginForm
+        ),
+        name="login",
+    ),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
